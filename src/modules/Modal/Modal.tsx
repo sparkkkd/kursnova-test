2c0dcd169350784fc2378fab0cd9e970610cf152
sparkkkd <roydog228@gmail.com>
2025-08-17 19:40:13 +0300

    Fix (Delete useLenisScrollLock hook)

--- src/modules/Modal/Modal.tsx
+++ src/modules/Modal/Modal.tsx
@@ -7,7 +7,6 @@ import { setIsModalOpen, setIsModalSuccess } from '../../store/slices/uiSlice'
 import { useWindowSize } from 'usehooks-ts'
 
 import styles from './Modal.module.sass'
-import { useLenisScrollLock } from '../../hooks/useLenisScrollLock'
 
 interface ModalProps extends React.PropsWithChildren {
 	className?: string
@@ -21,14 +20,9 @@ export const Modal: FC<ModalProps> = ({ className, children }) => {
 
 	const { width } = useWindowSize()
 
-	const { lock, unlock } = useLenisScrollLock()
-
 	useEffect(() => {
 		if (isModalOpen) {
 			setIsMounted(true)
-			lock()
-		} else {
-			unlock()
 		}
 	}, [isModalOpen])
 
